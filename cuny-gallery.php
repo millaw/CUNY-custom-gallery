24ffb6fd9cb4d82704a58583dbff236e06b642bc
Milla <milla.wynn@yahoo.com>
2025-05-16 09:07:11 -0400

    Update cuny-gallery.php

--- cuny-gallery.php
+++ cuny-gallery.php
@@ -3,7 +3,7 @@
  * Plugin Name: CUNY Gallery
  * Plugin URI: https://www.github.com/millawy/cuny-custom-gallery
  * Description: Gallery creation, image management, and frontend shortcode display. Insert a shortcode [cuny_gallery_X] into a page to display the gallery.
- * Version: 1.2.1
+ * Version: 1.3.0
  * Author: Milla Wynn
  * Author URI: https://www.github.com/millaw
  * License: GPL2
